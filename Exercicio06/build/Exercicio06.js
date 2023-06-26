cf61d4b8c322d4fc5deeb5197a3dbb4acc5f6b49
Fabricio <fabricio_lopes@outlook.com.br>
2023-06-26 12:45:18 -0300

    finalizando o exercicio 6

--- Exercicio06/build/Exercicio06.js
+++ Exercicio06/build/Exercicio06.js
@@ -1,14 +1,15 @@
 "use strict";
 function operations(num1, num2) {
     let sum = num1 + num2;
-    let diference = num1 - num2;
-    let multiplication = num2 * num2;
+    let difference = num1 - num2;
+    let multiplication = num1 * num2;
     let division = num1 / num2;
-    console.log(`Numeros recebidos: ${num1} e ${num2} 
+    let result = `Numeros recebidos: ${num1} e ${num2} 
     \nsoma: ${sum}
-    \ndiferença ${diference} 
-    \nmultiplicação ${multiplication} 
-    \ndivisão ${division} `);
+    \ndiferença: ${difference} 
+    \nmultiplicação: ${multiplication} 
+    \ndivisão: ${division}`;
+    return result;
 }
-operations(600, 45);
+console.log(operations(600, 45));
 //# sourceMappingURL=Exercicio06.js.map
--- Exercicio06/src/Exercicio06.ts
+++ Exercicio06/src/Exercicio06.ts
@@ -10,20 +10,22 @@
     \nmultiplicação: ${multiplication} 
     \ndivisão: ${division} `);
 }
+
+operations(600, 45);
 */
 function operations(num1:number, num2:number):string {
     let sum:number = num1 + num2;
-    let diference:number = num1 - num2;
-    let multiplication:number = num2 *num2;
+    let difference:number = num1 - num2;
+    let multiplication:number = num1 *num2;
     let division:number = num1 / num2;
 
-    return `Numeros recebidos: ${num1} e ${num2} 
+    let result = `Numeros recebidos: ${num1} e ${num2} 
     \nsoma: ${sum}
-    \ndiferença: ${diference} 
+    \ndiferença: ${difference} 
     \nmultiplicação: ${multiplication} 
     \ndivisão: ${division}`;
+
+    return result
 }
 
-
-
-operations(600, 45);
+console.log(operations(600, 45));
